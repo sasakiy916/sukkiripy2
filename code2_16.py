cd1f68c1aa3fa55cc57a1d9ead2862d860d1de3f
sasaki0719 <y.sasaki.roppongi@gmail.com>
2023-11-13 10:57:00 +0900

    教科書99p ディクショナリ

---
+++ code2_16.py
@@ -0,0 +1,11 @@
+scores = {"network":60,"database":80,"security":50}
+# del scores["security"]
+
+# score = scores.pop("network")
+# print(scores)
+# print(score)
+# list()
+# print(list(scores.keys()))
+print(scores.values())
+
+print(sum(scores.values()))
