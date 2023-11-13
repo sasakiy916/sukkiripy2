30f0587d4515e312235836ba0a03fff6d251db5f
sasaki0719 <y.sasaki.roppongi@gmail.com>
2023-11-13 14:27:52 +0900

    練習問題2_2 for文利用

--- ex2_2.py
+++ ex2_2.py
@@ -1,9 +1,7 @@
 scores = []
-scores.append(int(input("国語 >> ")))
-scores.append(int(input("算数 >> ")))
-scores.append(int(input("理科 >> ")))
-scores.append(int(input("社会 >> ")))
-scores.append(int(input("英語 >> ")))
+kamoku = ("国語","算数","理科","社会","英語")
+for k in kamoku:
+    scores.append(int(input(f"{k} >> ")))
 
 print(f"合計点:{sum(scores)}")
 print(f"平均点:{sum(scores)/len(scores)}")
